9f2cf6fedc4d7229978ca1cd60ff43982a341859
VkDigital29 <119841251+VkDigital29@users.noreply.github.com>
2022-12-07 11:00:16 +0530

    Update loaders.js

--- loaders.js
+++ loaders.js
@@ -1,3 +1,15 @@
- window.addEventListener('load', function () {
-        document.querySelector('.pre-loader').className += ' hidden';
-    });
+   document.onreadystatechange = function() {
+			setTimeout(function() {
+            if (document.readyState !== "complete") {
+                document.querySelector(
+                  "body").style.visibility = "hidden";
+                document.querySelector(
+                  "#preloader").style.visibility = "visible";
+            } else {
+                document.querySelector(
+                  "#preloader").style.display = "none";
+                document.querySelector(
+                  "body").style.visibility = "visible";
+            }
+			}, 3000);
+        };
