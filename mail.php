0e124311305d7c1cec77eb63acdb3f620f6ec214
VkDigital29 <119841251+VkDigital29@users.noreply.github.com>
2022-12-07 21:34:41 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -4,7 +4,7 @@ $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 $formcontent=" From: $name \n Email: $email \n Subject: $subject  \n Message: $message"; 
-$recipient = "kulkarnivishal308@gmail.com"; 
+$to = "kulkarnivishal308@gmail.com"; 
 $subject = "Contact Form"; 
 $mailheader = "From: $email \r\n"; 
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!"); 
