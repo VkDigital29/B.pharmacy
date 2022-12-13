bd2402c67566cf49e924e3595cdf6225278502ef
VkDigital29 <119841251+VkDigital29@users.noreply.github.com>
2022-12-13 12:02:00 +0530

    Update eduscript.js

--- eduscript.js
+++ eduscript.js
@@ -10,3 +10,7 @@ window.onscroll = () =>{
   menu.classList.remove('fa-times');
   navbar.classList.remove('active');
 }
+
+  document.addEventListener("contextmenu", function(e) {
+    e.preventDefault();
+  }, false);
