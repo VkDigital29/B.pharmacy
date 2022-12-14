7013b3bb5f6af3672475bbe2a6dc5b46760d6236
VkDigital29 <119841251+VkDigital29@users.noreply.github.com>
2022-12-14 11:48:33 +0530

    Update eduscript.js

--- eduscript.js
+++ eduscript.js
@@ -11,12 +11,31 @@ window.onscroll = () =>{
   navbar.classList.remove('active');
 }
 
-  document.addEventListener("contextmenu", function(e) {
-    e.preventDefault();
-    
-    alert("No!! No!!");
+window.alert = (text = "", theme = "danger") => {
+  const getElems = () => document.querySelectorAll(".custom_alert");
+  if (getElems().length)
+    Array.from(getElems()).map((e, i) => {
+      try {
+        i === 1 ? (e.innerHTML = `<div class="alert alert-${theme}" role="alert">${text}</div>`) : e.remove();
+        clearTimeout(window.alertTimeout_1);
+        clearTimeout(window.alertTimeout_2);
+      } catch {}
+    });
 
-  }, false);
+  document.body.insertAdjacentHTML("afterbegin", `<div class="custom_alert"><div class="alert alert-${theme}" role="alert">${text}</div></div>`);
+
+  window.alertTimeout_1 = setTimeout(() => {
+    const k = document.querySelector(".custom_alert");
+    if (k) {
+      k.classList.add("closed");
+      window.alertTimeout_2 = setTimeout(() => {
+        k.remove();
+      }, 500);
+    }
+  }, 2000);
+};
+
+
 const showOnPx = 100;
 const backToTopButton = document.querySelector(".back-to-top");
 const pageProgressBar = document.querySelector(".progress-bar");
