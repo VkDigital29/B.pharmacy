e0ac5027223500aa8ede085c07508bd741c66eea
VkDigital29 <119841251+VkDigital29@users.noreply.github.com>
2022-12-20 15:27:02 +0530

    Update eduscript.js

--- eduscript.js
+++ eduscript.js
@@ -86,15 +86,25 @@ backToTopButton.addEventListener("click", goToTop);
 
 
 
-const f = document.getElementById('form');
-const q = document.getElementById('query');
-const site = 'https://vkdigital29.github.io/B.pharmacy/';
 
-function submitted(event) {
-  event.preventDefault();
-  const url = site + '+' + q.value;
-  const win = window.open(url, '_blank');
-  win.focus();
-}
 
-f.addEventListener('submit', submitted);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
